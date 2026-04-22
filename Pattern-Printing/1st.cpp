5439e3c6fd0f308a1fb1ff2eb6ab86e674db10e1
Manish Kumar <technicalgyaani.in@gmail.com>
2026-04-22 23:23:55 +0530

    diamond star

---
+++ Pattern-Printing/1st.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int total_rows;
+    cin>>total_rows;
+
+    for(int row=1; row<=total_rows;row++){
+        //for space
+        for(int col=1;col<=total_rows-row;col++)
+            cout<<" ";
+
+        for(int col=1;col<=2*row-1;col++)
+            cout<<"*";
+
+        cout<<endl;
+    }
+    return 0;
+}
